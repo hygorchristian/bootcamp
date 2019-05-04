6d470e396319a79957e025abc1102f85d64b3568
Hygor Dias <hygor.christian@gmail.com>
2019-05-04 15:40:41 -0300

    adicionando middlewares de auth

--- node_2/src/routes.js
+++ node_2/src/routes.js
@@ -1,9 +1,23 @@
 const routes = require('express').Router()
 const UserController = require('./app/controllers/UserController')
+const SessionController = require('./app/controllers/SessionController')
 const multerConfig = require('./config/multer')
 const upload = require('multer')(multerConfig)
 
+const authMiddleware = require('./app/middlewares/auth')
+const guestMiddleware = require('./app/middlewares/guest')
+
+routes.use('/app', authMiddleware)
+
+routes.get('/', guestMiddleware, SessionController.create)
+routes.post('/signin', guestMiddleware, SessionController.store)
+
 routes.get('/signup', UserController.create)
 routes.post('/signup', upload.single('avatar'), UserController.store)
+
+routes.get('/app/dashboard', (req, res) => {
+  console.log(req.session.user)
+  res.render('dashboard')
+})
 
 module.exports = routes
