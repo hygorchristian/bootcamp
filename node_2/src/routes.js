cfd8397e4ae37a583f119e07a52e845e2bf3a2a2
Hygor Dias <hygor.christian@gmail.com>
2019-05-03 22:20:51 -0300

    criando formulario de signup

--- node_2/src/routes.js
+++ node_2/src/routes.js
@@ -1,7 +1,7 @@
 const routes = require('express').Router()
+const UserController = require('./app/controllers/UserController')
 
-routes.get('/', (req, res) => {
-  res.send('Hello')
-})
+routes.get('/signup', UserController.create)
+routes.post('/signup', UserController.store)
 
 module.exports = routes
