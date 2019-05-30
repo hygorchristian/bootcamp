d03b5275c06f45089d12cdff8ece771bb0f33a01
Hygor Dias <hygor.christian@gmail.com>
2019-05-30 00:29:30 -0300

    ajustando o redux-saga

---
+++ react/react-3/src/components/footer.js
@@ -0,0 +1,20 @@
+/* eslint-disable react/jsx-one-expression-per-line */
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+const Footer = ({ count }) => (
+  <p>
+    Você tem {count} favoritos
+  </p>
+);
+
+Footer.propTypes = {
+  count: PropTypes.number.isRequired
+};
+
+const mapStateToProps = ({ favorites }) => ({
+  count: favorites.length
+});
+
+export default connect(mapStateToProps)(Footer);
--- react/react-3/src/pages/main/index.js
+++ react/react-3/src/pages/main/index.js
@@ -1,5 +1,68 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import PropTypes from 'prop-types';
+import * as FavoritesActions from '../../store/actions/favorites';
+import Footer from '../../components/footer';
 
-const Main = () => <h1>Main</h1>;
+class Main extends Component {
+  static propTypes = {
+    addFavoriteRequest: PropTypes.func.isRequired,
+    favorites: PropTypes.arrayOf(PropTypes.shape({
+      id: PropTypes.number,
+      name: PropTypes.string,
+      description: PropTypes.string,
+      url: PropTypes.string
+    })).isRequired
+  }
 
-export default Main;
+  state = {
+    repositoryInput: ''
+  }
+
+  handleAddRepository = (e) => {
+    e.preventDefault();
+    this.props.addFavoriteRequest(this.state.repositoryInput);
+  };
+
+  render() {
+    const { repositoryInput } = this.state;
+    const { favorites } = this.props;
+    return (
+      <div>
+        <form onSubmit={this.handleAddRepository}>
+          <input
+            placeholder="usuário/repositório"
+            value={repositoryInput}
+            onChange={e => this.setState({ repositoryInput: e.target.value })}
+          />
+          <button type="submit">Adicionar</button>
+        </form>
+
+        <ul>
+          {
+            favorites.map(favorite => (
+              <li key={favorite.id}>
+                <p>
+                  <strong>{favorite.name}</strong>
+                  {' '}
+                  {favorite.description}
+                </p>
+                <a href={favorite.url} target='_blank'>Acessar</a>
+              </li>
+            ))
+          }
+        </ul>
+        <Footer />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = ({ favorites }) => ({
+  favorites
+});
+
+const mapDispatchToProps = dispatch => bindActionCreators(FavoritesActions, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Main);
