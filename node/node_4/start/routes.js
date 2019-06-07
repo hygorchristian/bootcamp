5f77b31a9a08211010da5887b2b1abb4f66725f8
Hygor Dias <hygor.christian@gmail.com>
2019-06-07 10:24:22 -0300

    criando files

--- node/node_4/start/routes.js
+++ node/node_4/start/routes.js
@@ -1,23 +1,12 @@
 'use strict'
 
-/*
-|--------------------------------------------------------------------------
-| Routes
-|--------------------------------------------------------------------------
-|
-| Http routes are entry points to your web application. You can create
-| routes for different URLs and bind Controller actions to them.
-|
-| A complete guide on routing is available here.
-| http://adonisjs.com/docs/4.0/routing
-|s
-*/
-
-/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
-Route.get('/', () => {
-  const elel = 1212
-  console.log(elel)
-  return { greeting: 'Hello world in JSON' }
-})
+Route.post('users', 'UserController.store')
+Route.post('sessions', 'SessionController.store')
+
+Route.post('passwords', 'ForgotPasswordController.store')
+Route.put('passwords', 'ForgotPasswordController.update')
+
+Route.post('files', 'FileController.store')
+Route.get('files/:id', 'FileController.show')
